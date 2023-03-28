f6981a73ea7cf0ee0752097de0a6ecd20563c27e
shephali09 <hiremathshephali9@gmail.com>
2023-03-28 23:02:25 +0530

    Add: Print welcome message

---
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbooksystem;
+
+//Print Welcome message
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book Program!");
+
+	}
+
+}
