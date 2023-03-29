250e81045fea5b2a87841d03fbcd84d8e9d63c97
shephali09 <hiremathshephali9@gmail.com>
2023-03-29 22:47:36 +0530

    Add: Ability to add a new contact to Address Book

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -1,5 +1,44 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBook extends Contacts {
+
+	public void addContacts() {
+		Scanner scanner = new Scanner(System.in);
+		Contacts contacts = new Contacts();
+
+		System.out.println("Enter First Name: ");
+		String firstName = scanner.nextLine();
+		contacts.setFirstName(firstName);
+
+		System.out.println("Enter Last Name: ");
+		String lastName = scanner.nextLine();
+		contacts.setLastName(firstName);
+
+		System.out.println("Enter Address: ");
+		String address = scanner.nextLine();
+		contacts.setAddress(address);
+
+		System.out.println("Enter City: ");
+		String city = scanner.nextLine();
+		contacts.setCity(city);
+
+		System.out.println("Enter State: ");
+		String state = scanner.nextLine();
+		contacts.setState(state);
+
+		System.out.println("Enter Email: ");
+		String email = scanner.nextLine();
+		contacts.setEmail(email);
+
+		System.out.println("Enter zip: ");
+		int zip = scanner.nextInt();
+		contacts.setZip(zip);
+
+		System.out.println("Enter Phone Number: ");
+		long phoneNumber = scanner.nextLong();
+		contacts.setPhoneNumber(phoneNumber);
+	}
 
 }
--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,12 +1,13 @@
 package com.bridgelabz.addressbooksystem;
 
-//Print Welcome message
-
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Address Book Program!");
+
+		AddressBook addressbook = new AddressBook();
+		addressbook.addContacts();
 
 	}
 
