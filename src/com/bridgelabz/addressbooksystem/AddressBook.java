bba265cba06c61cf47232047c4b6f9e591f70abd
shephali09 <hiremathshephali9@gmail.com>
2023-04-25 23:54:28 +0530

    Add: Ability to sort the entries in the address book alphabetically by Person's name

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Comparator;
 
 public class AddressBook extends Contacts {
 
